d1e832033af78e2295a1cb9b407c5f4cccfc69d6
Maayan Glikser <maayan@glikm.com>
2015-08-26 00:45:27 +0300

    Cleanup

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import ReactMinesweeper from 'react-minesweeper/app.jsx';
 let angularContainer = document.getElementById('angular');
 let reactContainer   = document.getElementById('react');
 
+// render angular to #angular dom node
 angular.bootstrap(angularContainer, [AngularMinesweeper.name]);
 
+// render react into #react dom node
 React.render(<ReactMinesweeper />, reactContainer);
